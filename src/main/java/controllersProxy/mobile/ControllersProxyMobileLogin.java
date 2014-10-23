253aac444b90c57ed08ddbab1dba7d0bb7a84795
raul <raul@raul-VAIO>
2014-10-23 20:16:58 +0200

    añadida interfaz publica de clase

--- src/main/java/controllersProxy/mobile/ControllersProxyMobileLogin.java
+++ src/main/java/controllersProxy/mobile/ControllersProxyMobileLogin.java
@@ -1,13 +1,27 @@
 package controllersProxy.mobile;
 
+import models.entities.User;
 import controllers.ControllerLogin;
 
 public class ControllersProxyMobileLogin implements ControllerLogin {
-
+    private User user;
+    
     @Override
     public void login() {
         // TODO Auto-generated method stub
         
     }
+    public ControllersProxyMobileLogin(User user) {
+        super();
+        this.user = user;
+    }
 
+    public ControllersProxyMobileLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public String process(){
+        return "viewLog";
+    }
 }
