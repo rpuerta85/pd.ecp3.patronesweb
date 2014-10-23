81cf6de7886f3da6d3ab16c86bb85c8f73daaf83
raul <raul@raul-VAIO>
2014-10-23 20:25:36 +0200

    añadida inerfaz pública para la clase "GenericDAO"

---
+++ src/main/java/models/DAO/GenericDAO.java
@@ -0,0 +1,13 @@
+package models.DAO;
+
+import java.util.List;
+
+public abstract class GenericDAO<T,ID> {
+    public abstract void create(T entity);
+    public abstract T read(ID id);
+    public abstract void update(T entity);
+    public abstract void delete(T entity);
+    public abstract void deleteByID(ID id);
+    public abstract List<T> findAll();
+    
+}
